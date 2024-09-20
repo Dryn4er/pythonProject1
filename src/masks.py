e3cd4652f1fd585a4d36f656e406243ee19a9be6
Andrey <mr.dryn4er@gmail.com>
2024-09-20 13:40:06 +0300

    new masks

--- src/masks.py
+++ src/masks.py
@@ -4,13 +4,13 @@ card_number = input()
 account_number = input()
 
 
-def get_mask_card_number(card_number: Union[int, str]) -> Union[str]:
+def get_mask_card_number(card_number: Union[int, str]) -> str:
     """Функция, которая маскирует номер карты"""
 
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
-def get_mask_account(account_number: Union[int, str]) -> Union[str]:
+def get_mask_account(account_number: Union[int, str]) -> str:
     """Функция, которая маскирует номер счета"""
 
     return f"**{account_number[-4:]}"
