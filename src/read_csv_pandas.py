4635e55f3462bd29d5f731346ca285c0a32c526d
Andrey <mr.dryn4er@gmail.com>
2024-10-29 13:26:24 +0300

    test homework questions

--- src/read_csv_pandas.py
+++ src/read_csv_pandas.py
@@ -5,10 +5,10 @@ import csv
 def read_csv(file_name: str) -> list:
     """Функция возвращает список словарей из файла csv"""
     try:
-        reader_csv = pd.read_csv(file_name, delimiter=';')
+        reader_csv = pd.read_csv(file_name, delimiter=";")
     except FileNotFoundError:
         return []
-    return reader_csv.to_dict(orient='records')
+    return reader_csv.to_dict(orient="records")
 
 
 def read_xlsx(file_name: str) -> list:
@@ -17,10 +17,10 @@ def read_xlsx(file_name: str) -> list:
         reader_xlsx = pd.read_excel(file_name)
     except FileNotFoundError:
         return []
-    return reader_xlsx.to_dict(orient='records')
+    return reader_xlsx.to_dict(orient="records")
 
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 
 #    print(read_csv("../transactions.csv"))
 #    print(read_xlsx('../transactions_excel (1).xlsx'))
